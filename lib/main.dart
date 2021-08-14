785603ed7d7c90c542f61ad555805cdebcc4263d
ruanvalente <ruanopensuselinux@gmail.com>
2021-08-14 18:49:13 -0300

    feat: added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/screens/home.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
+import 'package:payflow/screens/home.dart';
+import 'package:payflow/modules/splash_screen.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(AppWidget());
 }
 
-class MyApp extends StatelessWidget {
+class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'PayFlow',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.amber,
+        primaryColor: AppColors.primary,
       ),
-      home: Home(),
+      home: SplashScreen(),
     );
   }
 }
