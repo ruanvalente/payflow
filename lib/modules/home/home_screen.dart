3d1f0ae4744aeaad1e382ac39b13b27eedde6f62
ruanvalente <ruanopensuselinux@gmail.com>
2021-09-14 16:53:55 -0300

    feature: added home screen and bottom navigator

--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'package:payflow/modules/home/home_controller.dart';
+
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_style.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -8,11 +13,96 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final homeController = HomeController();
+  final pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(color: Colors.green)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Payflow'),
+      appBar: PreferredSize(
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text.rich(
+                TextSpan(
+                  text: 'Olá, ',
+                  style: TextStyles.titleRegular,
+                  children: [
+                    TextSpan(
+                      text: 'User',
+                      style: TextStyles.titleBoldBackground,
+                    )
+                  ],
+                ),
+              ),
+              subtitle: Text(
+                'Mantenha suas contas em dia',
+                style: TextStyles.captionShape,
+              ),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(152),
+      ),
+      body: pages[homeController.currentPage],
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {
+                  setState(() {
+                    homeController.setCurrentPage(0);
+                  });
+                },
+                color: AppColors.primary,
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('oi');
+                },
+                child: Container(
+                  width: 56,
+                  height: 56,
+                  decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Icon(
+                    Icons.add_box_outlined,
+                    color: AppColors.shape,
+                  ),
+                ),
+              ),
+              IconButton(
+                  icon: Icon(Icons.description_outlined),
+                  onPressed: () {
+                    setState(() {
+                      homeController.setCurrentPage(1);
+                    });
+                  },
+                  color: AppColors.body),
+            ],
+          ),
+        ),
       ),
     );
   }
